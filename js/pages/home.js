e54c9f89e07f94f6b54bf23aa17b3d00326898e9
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-12 01:24:53 +0530

    Add separate Indo home page module

---
+++ js/pages/home.js
@@ -0,0 +1,14 @@
+export function renderHomePage(container) {
+  container.innerHTML = `
+    <main class="indo-app">
+      <header class="indo-header">
+        <h1>Indo</h1>
+      </header>
+
+      <section class="indo-content">
+        <h2>Movies, Videos & Reels</h2>
+        <p>Indo web app foundation is ready.</p>
+      </section>
+    </main>
+  `;
+}
