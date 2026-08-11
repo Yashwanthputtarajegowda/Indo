3d64480f6c14ee1bd991cc1d9d490d3fad472060
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-12 01:48:01 +0530

    Correct bottom navigation to Reels Following Profile

--- js/components/bottom-navigation.js
+++ js/components/bottom-navigation.js
@@ -1,11 +1,29 @@
 export function renderBottomNavigation(container) {
   container.innerHTML = `
     <nav class="bottom-navigation" aria-label="Main navigation">
-      <button type="button" data-route="home">Home</button>
-      <button type="button" data-route="reels">Reels</button>
-      <button type="button" data-route="create">Create</button>
-      <button type="button" data-route="following">Following</button>
-      <button type="button" data-route="library">Library</button>
+      <button type="button" data-route="home">
+        <span aria-hidden="true">⌂</span>
+        <small>Home</small>
+      </button>
+
+      <button type="button" data-route="reels">
+        <span aria-hidden="true">▶</span>
+        <small>Reels</small>
+      </button>
+
+      <button type="button" data-route="create" aria-label="Create">
+        <span aria-hidden="true">+</span>
+      </button>
+
+      <button type="button" data-route="following">
+        <span aria-hidden="true">♡</span>
+        <small>Following</small>
+      </button>
+
+      <button type="button" data-route="profile">
+        <span aria-hidden="true">◯</span>
+        <small>Profile</small>
+      </button>
     </nav>
   `;
 }
