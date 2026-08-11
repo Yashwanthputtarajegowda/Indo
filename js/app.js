4b6f230fe18a2199b6c44bc25635d37e6e9be928
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-12 04:41:55 +0530

    Connect Messages list to Chat screen

--- js/app.js
+++ js/app.js
@@ -28,6 +28,27 @@ window.addEventListener("indo:create-account", () => {
   navigate(app, "home");
 });
 
+window.addEventListener("indo:video-open", (event) => {
+  navigate(app, "video-player", {
+    video: event.detail || {}
+  });
+});
+
+window.addEventListener("indo:message-open", (event) => {
+  navigate(app, "chat", {
+    thread: event.detail || {}
+  });
+});
+
+window.addEventListener("indo:new-message", () => {
+  navigate(app, "chat", {
+    thread: {
+      userId: "@new_user",
+      preview: "Start a new conversation"
+    }
+  });
+});
+
 window.addEventListener("indo:profile-updated", () => {
   if (document.querySelector(".profile-page")) {
     navigate(app, "profile");
