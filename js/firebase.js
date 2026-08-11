1f04ff30dc374f8401d814ca1fbd2a2264dbf3e0
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-11 10:18:11 +0530

    Fix Firebase web configuration

--- js/firebase.js
+++ js/firebase.js
@@ -5,6 +5,7 @@ import { getDatabase } from 'https://www.gstatic.com/firebasejs/12.6.0/firebase-
 const firebaseConfig = {
   apiKey: 'AIzaSyDXnkQ3JrGu44HJxs6-Rflhxkqnh0V8Kw',
   authDomain: 'indo-174f0.firebaseapp.com',
+  databaseURL: 'https://indo-174f0-default-rtdb.firebaseio.com',
   projectId: 'indo-174f0',
   storageBucket: 'indo-174f0.firebasestorage.app',
   messagingSenderId: '943630428817',
