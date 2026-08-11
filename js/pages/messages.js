045710e88c9c41dfedf8829071ab9d179e760fbe
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-11 21:26:57 +0530

    Format messages.js line by line

--- js/pages/messages.js
+++ js/pages/messages.js
@@ -1,1 +1,17 @@
-const list=document.querySelector('#chatList');const users=['User One','User Two','User Three'];list.innerHTML=users.map(u=>`<div class="chat"><b>${u}</b><br><small>Tap to open chat</small></div>`).join('');
+const list = document.querySelector('#chatList');
+
+const users = [
+    'User One',
+    'User Two',
+    'User Three'
+];
+
+list.innerHTML = users
+    .map((user) => `
+        <div class="chat">
+            <b>${user}</b>
+            <br>
+            <small>Tap to open chat</small>
+        </div>
+    `)
+    .join('');
