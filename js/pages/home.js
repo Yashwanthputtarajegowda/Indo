0cae635933e2bf243d2078d45dce44ac31931e12
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-11 22:32:29 +0530

    Add Home navigation routing

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,1 +1,30 @@
-console.log('Indo Home page loaded');
+const navigationButtons = document.querySelectorAll(
+    '.nav-button'
+);
+
+function openPage(page) {
+    if (page === 'home') {
+        window.location.href = '../index.html';
+        return;
+    }
+
+    if (page === 'messages') {
+        window.location.href = './messages.html';
+        return;
+    }
+
+    if (page === 'reels') {
+        window.location.href = './reels.html';
+        return;
+    }
+
+    if (page === 'profile') {
+        window.location.href = './profile.html';
+    }
+}
+
+navigationButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        openPage(button.dataset.page);
+    });
+});
