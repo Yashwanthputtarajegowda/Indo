0ad792c7dbb4c18b4687e24c6204cec0b284b88d
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-11 20:58:59 +0530

    Create Messages page logic

---
+++ js/pages/messages.js
@@ -0,0 +1,1 @@
+const list=document.querySelector('#chatList');const users=['User One','User Two','User Three'];list.innerHTML=users.map(u=>`<div class="chat"><b>${u}</b><br><small>Tap to open chat</small></div>`).join('');
