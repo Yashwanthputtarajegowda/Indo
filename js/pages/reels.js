96d2f756fb5f483dff3fa6c57fc148e4ffc46b95
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-12 01:32:03 +0530

    Add separate Reels page foundation

---
+++ js/pages/reels.js
@@ -0,0 +1,13 @@
+export function renderReelsPage(container) {
+  container.innerHTML = `
+    <main class="indo-page">
+      <header class="indo-header">
+        <h1>Reels</h1>
+      </header>
+
+      <section class="indo-content">
+        <p>Reels will appear here.</p>
+      </section>
+    </main>
+  `;
+}
