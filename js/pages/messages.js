f7f6bbc6488b63e8627689cde5ec448ec1a6bf88
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-11 22:26:54 +0530

    Fix Messages navigation buttons and page routing

--- js/pages/messages.js
+++ js/pages/messages.js
@@ -1,4 +1,5 @@
 const list = document.querySelector('#chatList');
+const navigationButtons = document.querySelectorAll('.nav-button');
 
 const users = [
     'User One',
@@ -6,12 +7,45 @@ const users = [
     'User Three'
 ];
 
-list.innerHTML = users
-    .map((user) => `
-        <div class="chat">
-            <b>${user}</b>
-            <br>
-            <small>Tap to open chat</small>
-        </div>
-    `)
-    .join('');
+function renderChats() {
+    list.innerHTML = users
+        .map((user) => {
+            return `
+                <div class="chat">
+                    <b>${user}</b>
+                    <br>
+                    <small>Tap to open chat</small>
+                </div>
+            `;
+        })
+        .join('');
+}
+
+function openPage(page) {
+    if (page === 'home') {
+        window.location.href = '../index.html';
+        return;
+    }
+
+    if (page === 'messages') {
+        window.location.href = './messages.html';
+        return;
+    }
+
+    if (page === 'reels') {
+        window.location.href = './reels.html';
+        return;
+    }
+
+    if (page === 'profile') {
+        window.location.href = './profile.html';
+    }
+}
+
+navigationButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        openPage(button.dataset.page);
+    });
+});
+
+renderChats();
