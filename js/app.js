d45eba5d0da4245e66b255f7d3cdbb91c58178f3
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-12 04:35:39 +0530

    Start account activity tracking from app auth state

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 import { renderFlashPage } from "./pages/flash.js";
+import { watchAuthState } from "./services/firebase-auth.js";
+import { startActivityTracking } from "./services/activity.js";
 
 const app = document.querySelector("#app");
 
@@ -8,5 +10,16 @@ flashStyles.rel = "stylesheet";
 flashStyles.href = "./css/flash.css";
 
 document.head.appendChild(flashStyles);
+
+const activityTracker = startActivityTracking();
+
+watchAuthState((user) => {
+  if (user) {
+    activityTracker.start();
+    return;
+  }
+
+  activityTracker.stop();
+});
 
 renderFlashPage(app);
