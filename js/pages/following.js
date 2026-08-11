a2ad446d9c490a31e9b4201063d7f99e79ef23dd
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-12 01:35:23 +0530

    Add separate Following page foundation

---
+++ js/pages/following.js
@@ -0,0 +1,13 @@
+export function renderFollowingPage(container) {
+  container.innerHTML = `
+    <main class="indo-page">
+      <header class="indo-header">
+        <h1>Following</h1>
+      </header>
+
+      <section class="indo-content">
+        <p>Posts and videos from people you follow will appear here.</p>
+      </section>
+    </main>
+  `;
+}
