d2e80e9752bd6621007cd55372cec1e9945da605
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-12 07:04:56 +0530

    fix: update Firebase web config

--- js/services/firebase-auth.js
+++ js/services/firebase-auth.js
@@ -7,13 +7,14 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBv8eQ9rX8xQ8oY0dYqKqf9mG7pKq7r7xY",
+  apiKey: "AIzaSyDXnkQ3JrBGu44HJxs6-Rflhxkqnh0V8Kw",
   authDomain: "indo-174f0.firebaseapp.com",
   databaseURL: "https://indo-174f0-default-rtdb.firebaseio.com",
   projectId: "indo-174f0",
   storageBucket: "indo-174f0.firebasestorage.app",
-  messagingSenderId: "1053729027185",
-  appId: "1:1053729027185:web:8b2d0a9a7c0b1e9f4c2d11"
+  messagingSenderId: "943630428817",
+  appId: "1:943630428817:web:61a8152dfa4549f5f0ed30",
+  measurementId: "G-TVK92L9S1R"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
