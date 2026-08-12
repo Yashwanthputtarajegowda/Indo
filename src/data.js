375cbd153c7c826719fd676a180de76f3d3bfa5a
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-12 17:59:28 +0530

    Add shared app data

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const samplePosts = [
+  { user: 'Yashwanth', id: '@yash123', image: 'https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?auto=format&fit=crop&w=900&q=80', likes: '142', comments: '12', caption: 'Nature is peace ✨' },
+  { user: 'Arjun', id: '@arjun31', image: 'https://images.unsplash.com/photo-1524504388940-b1c1722653e1?auto=format&fit=crop&w=900&q=80', likes: '98', comments: '8', caption: 'Keep moving forward.' }
+];
+
+export const icons = {
+  home: '⌂', search: '⌕', reel: '▶', create: '+', profile: '●', heart: '♡', comment: '○', share: '➤', bookmark: '♧', bell: '♧', settings: '⚙', back: '‹', more: '⋯'
+};
