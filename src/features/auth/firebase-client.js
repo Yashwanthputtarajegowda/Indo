7c454b62dd6546557e76087fe2cb3a2353483d01
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-12 18:08:29 +0530

    Add Firebase client authentication

---
+++ src/features/auth/firebase-client.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDXnkQ3JrBGu44HJxs6-Rflhxkqnh0V8Kw',
+  authDomain: 'indo-174f0.firebaseapp.com',
+  databaseURL: 'https://indo-174f0-default-rtdb.firebaseio.com',
+  projectId: 'indo-174f0',
+  storageBucket: 'indo-174f0.firebasestorage.app',
+  messagingSenderId: '943630428817',
+  appId: '1:943630428817:web:61a8152dfa4549f5f0ed30'
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseApp);
+export { createUserWithEmailAndPassword, signInWithEmailAndPassword };
