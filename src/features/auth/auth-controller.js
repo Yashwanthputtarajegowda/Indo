065a0023439629096f63f56b6f736139c865dfd0
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-14 23:19:00 +0530

    Bump auth controller cache for User ID behavior

--- src/features/auth/auth-controller.js
+++ src/features/auth/auth-controller.js
@@ -1,7 +1,7 @@
 import { state } from '../../state.js';
 import { submitSignup } from './signup-form.js';
 
-const ROUTER_VERSION = '20260814-143';
+const ROUTER_VERSION = '20260814-146';
 
 function getRoot() {
   return document.getElementById('root');
