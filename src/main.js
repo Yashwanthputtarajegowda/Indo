76ce6ab9fff226f4398a05e4dcb94ba93cb9550f
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-14 23:09:00 +0530

    Bump app cache version for auth update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { bindAuthSwitches, bindSignupForm } from './features/auth/auth-controller.js';
 
 const app = document.getElementById('root');
-const ROUTER_VERSION = '20260814-143';
+const ROUTER_VERSION = '20260814-144';
 let navigationBusy = false;
 
 async function navigate(screen){
