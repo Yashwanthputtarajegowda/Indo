17e65a2efc678a6551b74de8aa1cf59cd0b37942
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-14 23:05:55 +0530

    Bump app cache for unrestricted User ID signup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { bindAuthSwitches, bindSignupForm } from './features/auth/auth-controller.js';
 
 const app = document.getElementById('root');
-const ROUTER_VERSION = '20260814-142';
+const ROUTER_VERSION = '20260814-143';
 let navigationBusy = false;
 
 async function navigate(screen){
