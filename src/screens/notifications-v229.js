0f19b6c2985e4811cc2ad9abf69466e7c7e0e28e
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-15 05:36:18 +0530

    Full replacement v229: clear notification badge immediately after read

---
+++ src/screens/notifications-v229.js
@@ -0,0 +1,8 @@
+import { renderNotifications as renderBaseNotifications } from './notifications.js?v=229';
+
+export async function renderNotifications(app, mode='all'){
+  await renderBaseNotifications(app,mode);
+  if(mode==='all'){
+    window.dispatchEvent(new CustomEvent('indo:notifications-read'));
+  }
+}
