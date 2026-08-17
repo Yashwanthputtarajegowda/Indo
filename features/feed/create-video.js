6d89d924c94a56cbf0e53e514d65baf093263dcf
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-17 08:55:07 +0530

    fix: refresh upload module after endpoint correction

--- features/feed/create-video.js
+++ features/feed/create-video.js
@@ -1,5 +1,5 @@
 import { auth } from "../auth/firebase-client.js";
-import { uploadVideoToTelegram } from "../upload/telegram-upload.js?v=20260817-telegram-upload-v5";
+import { uploadVideoToTelegram } from "../upload/telegram-upload.js?v=20260817-telegram-upload-v6";
 
 const MAX_VIDEO_BYTES = 50 * 1024 * 1024;
 
