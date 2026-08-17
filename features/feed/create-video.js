2246548160a13f2b1837613905115e0688417c60
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-17 08:48:19 +0530

    fix: refresh video uploader cache version

--- features/feed/create-video.js
+++ features/feed/create-video.js
@@ -1,5 +1,5 @@
 import { auth } from "../auth/firebase-client.js";
-import { uploadVideoToTelegram } from "../upload/telegram-upload.js?v=20260817-telegram-upload-v4";
+import { uploadVideoToTelegram } from "../upload/telegram-upload.js?v=20260817-telegram-upload-v5";
 
 const MAX_VIDEO_BYTES = 50 * 1024 * 1024;
 
