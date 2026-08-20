768cbaece217e8edf84e0ba2addeed8581467a4c
Yashwanthputtarajegowda <yashwanthputtarajegowda@gmail.com>
2026-08-20 16:53:18 +0530

    Bump service worker cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "indo-shell-v5";
+const CACHE_NAME = "indo-shell-v6";
 
 self.addEventListener("install", () => {
   self.skipWaiting();
@@ -8,9 +8,7 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches
       .keys()
-      .then((keys) =>
-        Promise.all(keys.map((key) => caches.delete(key))),
-      )
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
       .then(() => self.clients.claim()),
   );
 });
